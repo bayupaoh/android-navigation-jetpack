2938f39ff9086e577f2a455c0819227bba9eb4fe
bayupaoh <bayupaoh@gmail.com>
2019-02-04 14:02:51 +0700

    add navigation drawer

--- app/src/main/java/com/suitmedia/bayunavigation/MainActivity.kt
+++ app/src/main/java/com/suitmedia/bayunavigation/MainActivity.kt
@@ -7,17 +7,27 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var appBarConfiguration : AppBarConfiguration ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolBar)
 
+        setSupportActionBar(toolBar)
+        appBarConfiguration = AppBarConfiguration(navOverMainActivity.findNavController().graph, drawer_layout)
+        setupActionBarWithNavController(navOverMainActivity.findNavController(), appBarConfiguration!!)
+
         bottomNavMainActivity.setupWithNavController(navOverMainActivity.findNavController())
+        navigation_view.setupWithNavController(navOverMainActivity.findNavController())
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navOverMainActivity.findNavController().navigateUp(appBarConfiguration!!) || super.onSupportNavigateUp()
+    }
+
 }
